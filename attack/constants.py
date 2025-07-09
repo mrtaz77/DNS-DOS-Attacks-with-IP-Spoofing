5e6c3ffbe79347a7c75d9c2928aea364ac0ec00b
mrtaz77 <mrtazwar23@gmail.com>
2025-07-09 19:47:54 +0600

    removing some attacks

--- attack/constants.py
+++ attack/constants.py
@@ -1,8 +1,5 @@
 from enum import Enum
 
 class Attack(Enum):
-  TCP_SYN_FLOOD = 1
-  ICMP_PING_FLOOD = 2
-  ICMP_SMURF_ATTACK = 3
-  UDP_FRAGGLE_ATTACK = 4
-  UDP_FRAGMENT_FLOOD = 5
+  UDP_FRAGGLE_ATTACK = 1
+  UDP_FRAGMENT_FLOOD = 2
